a10c875d2cb272f4d217e9a69c80a014a6145ee1
GadeHasika <160099681+GadeHasika@users.noreply.github.com>
2025-10-17 14:42:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,5 +48,6 @@ def home():
     return render_template("index.html", submitted=False, quiz=list(enumerate(quiz)))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port="5001",debug=True)
+    app.run(host='0.0.0.0',port="5000",debug=True)
+
 
